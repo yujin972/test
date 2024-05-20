f9f81d7837c16cf0fa5e6af88a213948fed508f0
yujin972 <106063537+yujin972@users.noreply.github.com>
2024-05-20 17:44:26 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("/include/header.php"); ?>
+<?php include("./include/header.php"); ?>
+<?php include("../include/header.php"); ?>
+
+<div class="wrap" id="main">
+
+    <!-- 메인 첫 화면 영역 //-->
+    <section>
+        <div class="container">
+메인입니다
+        </div>
+    </section>
+
+</div>
+
+
+<?php include("../include/footer.php"); ?>
