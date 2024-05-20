c0e9c99dbb8d5ab547c31efc2bcfbb1e250916c2
yujin972 <106063537+yujin972@users.noreply.github.com>
2024-05-20 17:45:52 +0900

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, shrink-to-fit=no, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<title>포트폴리오</title>
+<!--<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">-->
+<link rel="stylesheet" href="/css/style.css">
+<!--<link rel="stylesheet" href="/css/swiper.css">-->
+<script src="/js/jquery-3.6.0.js"></script>
+<!--<script src="/js/swiper.jquery.min.js"></script>-->
+<script src="/js/mobile.js"></script>
+<script src="/js/style.js"></script>
+<script src="/js/newsticker.js"></script>
+
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="웹퍼블리셔 강유진 포트폴리오">
+<meta property="og:url" content="http://ssapp.nayo.kr">
+<meta property="og:title" content="웹퍼블리셔 강유진 포트폴리오">
+<meta property="og:image" content="http://ssapp.nayo.kr/images/og.png">
+<meta name="twitter:url" content="http://ssapp.nayo.kr/">
+<meta name="twitter:title" content="웹퍼블리셔 강유진 포트폴리오">
+<meta name="twitter:card" content="photo">
+<meta name="twitter:image" content="http://ssapp.nayo.kr/images/og.png">
+<link rel="canonical" href="http://ssapp.nayo.kr/">
+<link rel="apple-touch-icon-precomposed" href="http://ssapp.nayo.kr/images/favi/apple-icon-72x72.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://ssapp.nayo.kr/images/favi/apple-icon-144x144.png">
+<link rel="shortcut icon" href="http://ssapp.nayo.kr/images/favi/favicon.ico">
+<link rel="icon" href="http://ssapp.nayo.kr/images/favi/favicon.ico">
+<meta property="og:description" content="웹 퍼블리셔 강유진의 포트폴리오입니다" />
+<meta name="description" content="웹 퍼블리셔 강유진의 포트폴리오입니다" />
+<link rel="canonical" href="http://ssapp.nayo.kr/" />
+
+</head>
+
+<body>
+	
+<div id="body_wrap">
+	
+	<!-- Header -->
+	<header id="header">
+        <div class="container flex jcb aic">
+헤더
+            <!--왼쪽//-->
+            <h1 id="logo">
+                <a href="/"><img class="of_ct" src="/images/logo.svg" alt="솔라솔루션"></a>
+            </h1>
+
+            <!--오른쪽//-->
+            <ul class="flex">
+                <li id="alarm"><a href="/pages/04_notice/page_2_01.html"><img class="of_ct" src="/images/ico/alarm.svg" alt="알림"/></a></li>
+                <li id="menu"><img class="of_ct" src="/images/ico/menu.svg" alt="메뉴"/></a></li>
+            </ul>
+            
+        </div>
+    </header>
+	<!-- // Header -->
+	
